b38f48958eeca10b55349ed620a2144d55f1e062
Don Deckard <don_deckard@yahoo.com>
2016-07-08 00:50:31 -0400

    Added variables to tileset

--- tileset.rb
+++ tileset.rb
@@ -1,9 +1,14 @@
 class Tileset
 #tile array
+tiles = Array.new 
 #tile count
+tilecount = 0
 #tile set filename
+filename = ""
 #tile width
+width = 32
 #tile height
+height = 0
 #load tileset
 #get tile
 
